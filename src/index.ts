5942c0bd6bb33dcad42e8093dd4cddc9ae42726f
Analía Alvado <104107403+Analia1961@users.noreply.github.com>
2022-05-17 20:30:54 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+// CLASE 4 - Tarea 4 - Viernes 29 de abril 2022
+// Estructura de Control - Selección
+
+// Ejercicio – Aumento de Sueldo
+
+/* • Una empresa desea premiar a sus empleados con un aumento de 
+sueldo. Este aumento se ajusta a la siguiente tabla:
+Actual Sueldo con Aumento
+0 - 15.000 $ 20%
+15.001 - 20.000 $ 10%
+20.001 - 25.000 $ 5%
+Más de 25.000 $ No hay aumento
+
+• Desarrolle un algoritmo dado el salario actual de un empleado
+ determine el aumento de sueldo a aplicar y se lo muestre */
+
+let dato = document.getElementById("dato");
+
+rotulo.innerHTML = "Ingrese el salario actual del empleado: ";
+
+let nuevoSueldo: number = 0;
+
+let btnEnviar = document.getElementById("btnEnviar");
+
+btnEnviar.addEventListener("click", () => {
+  let sueldoActual: number = Number(dato.value);
+
+  if (0 < sueldoActual && sueldoActual <= 15000) {
+    nuevoSueldo = sueldoActual * 1.2;
+    console.log("Ud. tiene un aumento de 20%. Nuevo Sueldo: $" + nuevoSueldo);
+  } else if (15001 < sueldoActual && sueldoActual <= 20000) {
+    nuevoSueldo = sueldoActual * 1.1;
+    console.log("Ud. tiene un aumento de 10%. Nuevo Sueldo: $" + nuevoSueldo);
+  } else if (20001 < sueldoActual && sueldoActual <= 25000) {
+    nuevoSueldo = sueldoActual * 1.05;
+    console.log("Ud. tiene un aumento de 5%. Nuevo Sueldo: $" + nuevoSueldo);
+  } else {
+    nuevoSueldo = sueldoActual;
+    console.log("Ud. no tiene aumento");
+  }
+});
